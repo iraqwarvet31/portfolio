1db9cde96cead723e00ca23b60971d5b7b0498bc
Larry Olguin <iraqwarvet31@gmail.com>
2021-04-11 11:12:04 -0700

    create initial webpack boilerplate

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        REACT IS RUNNING
+      </div>
+    )
+  }
+}
+
+export default App;
