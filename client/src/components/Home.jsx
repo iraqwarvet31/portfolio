d0adb8786ee4727aac1d27ab2c71252e500c5659
Larry Olguin <iraqwarvet31@gmail.com>
2021-04-13 22:36:52 -0700

    add particle.js library and intro text

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import Particles from 'react-particles-js';
+import ParticlesOptions from '../../particles.json'
+
 const Home = (props) => (
-  <div className="home">Background</div>
+  <div className="home">
+    <div className="flex">
+      <div className="text">
+        Hello, I'm
+        <span className="highlight">Larry Olguin</span>
+        <br />
+        I'm a full-stack web developer
+      </div>
+    </div>
+    <Particles
+      params={ParticlesOptions}
+    />
+  </div>
 )
 
 
