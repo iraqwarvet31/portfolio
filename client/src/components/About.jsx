0af5b4bf2687435742530f3e8b8fc4639dfd8396
Larry Olguin <iraqwarvet31@gmail.com>
2021-04-17 14:54:27 -0700

    add skills and icon container and css styles

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -8,6 +8,16 @@ const About = (props) => {
     <div className="about">
       <div className="container">
         <h1>About page</h1>
+        <div className="skills-container">
+          <div className="skill-container">
+            <div className="icon-container">
+              <span class="iconify" data-icon="mdi-speedometer" data-inline="false"></span>
+            </div>
+            <div className="text-container">
+
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
