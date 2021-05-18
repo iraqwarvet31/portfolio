dd06b1c8e6263e25e4c69163c283cd86d137be61
Larry Olguin <iraqwarvet31@gmail.com>
2021-05-18 15:11:58 -0700

    removed test code

--- server/index.js
+++ server/index.js
@@ -4,54 +4,6 @@ const cors = require('cors');
 const { google } = require('googleapis');
 const nodemailer = require('nodemailer');
 
-// let app = express();
-// let port = 3000;
-// require('dotenv').config();
-
-// app.use(express.static(__dirname + '/../public'));
-// app.use(express.json());
-// app.use('/', router);
-
-// // Authentication to my gmail account
-// const contactEmail = nodemailer.createTransport({
-//   host: 'smtp.gmail.com',
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: process.env.GMAIL_USER,
-//     pass: process.env.GMAIL_PASS,
-//   },
-// });
-
-// // Verifiy gmail connection
-// contactEmail.verify((error) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Form ready to send emails to byteTheBits0101@gmail.com');
-//   }
-// })
-
-// router.post('/contact', (req, res) => {
-//   const {name, email, message} = req.body;
-
-//   const mail = {
-//     from: name,
-//     to: 'byteTheBits0101@gmail.com',
-//     subject: 'Contact Form Message',
-//     html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
-//   };
-
-//   contactEmail.sendMail(mail, (error) => {
-//     if (error) {
-//       res.json({status: 'failed'});
-//     } else {
-//       res.json({status: 'sent'});
-//     }
-//   });
-// });
-
-
 let app = express();
 let port = 3000;
 require('dotenv').config();
